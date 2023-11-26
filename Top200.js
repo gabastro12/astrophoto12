fd1bdead9e19ff68bd84bfff2326d28059b39303
gabastro12 <69004698+gabastro12@users.noreply.github.com>
2023-11-26 22:43:35 +0100

    Update Top200.js

--- Top200.js
+++ Top200.js
@@ -13,37 +13,56 @@ window.addEventListener("load", function () {
     var sectionNames = ["lion", "com", "peLion", "crB", "balance", "corbeau", "coupe", "bouvier", "sextant", "chienChasse", "hydre", "vierge", "amasVierge", "aigle", "fleche", "sagittaire", "scorpion", "lyre", "cygne", "sobieski", "dauphin", "serpent", "ophiuchus", "hercule", "peRenard", "andromede", "lezard", "austral", "verseau", "belier", "sculpteur", "triangle", "capricorne", "pegase", "persee", "baleine", "poissons", "lievre", "lynx", "cocher", "cancer", "taureau", "grandChien", "eridan", "gemeaux", "orion", "licorne", "girafe", "grOurse", "poupe", "cassiopee", "peOurse", "cephee", "dragon"];
 
     // Fonction pour compter les images dans une section donnée
-    function countImagesInSection(section) {
-        var count = 0;
+    // Définir la fonction countImagesInSection en premier
+function countImagesInSection(section) {
+    var count = 0;
+    var totalImages = 0;
 
-        section.forEach(function (element) {
-            var images = element.querySelectorAll("img");
+    section.forEach(function (element) {
+        var images = element.querySelectorAll("img");
 
-            images.forEach(function (image) {
-                if (image.naturalWidth > 300) {
-                    count++;
-                }
-            });
+        images.forEach(function (image) {
+            totalImages++;
+
+            // Vérifiez si le nom de l'image se termine par "-a.jpg"
+            if (image.naturalWidth > 300 && image.src.endsWith("-a.jpg")) {
+                count++;
+            }
         });
+    });
 
-        return count;
-    }
-
-    // Utilisez une boucle pour traiter toutes les sections
-    for (var i = 0; i < sectionNames.length; i++) {
-        var currentSectionName = sectionNames[i];
-        var currentSection = document.querySelectorAll('.section[data-section="' + currentSectionName + '"]');
-        var currentCount = countImagesInSection(currentSection);
-
-        // Affichez les résultats dans le HTML
-var currentResultElement = document.getElementById(currentSectionName + '-result');
-if (currentResultElement) {
-    currentResultElement.textContent += " " + currentCount + " sur x";
-} else {
-    console.error("L'élément avec l'ID " + currentSectionName + "-result n'a pas été trouvé.");
+    // Ne comptez que si au moins une image de la paire a le suffixe "-a.jpg"
+    return { count: count, total: totalImages };
 }
 
-        // Affichez les résultats dans la console
-        console.log("Nombre - section " + currentSectionName + " : " + currentCount);
+// Déclarer la variable totalImages en dehors de la boucle
+var totalImages = 0;
+
+// Utilisez une boucle pour traiter toutes les sections
+for (var i = 0; i < sectionNames.length; i++) {
+    var currentSectionName = sectionNames[i];
+    var currentSection = document.querySelectorAll('.section[data-section="' + currentSectionName + '"]');
+    var counts = countImagesInSection(currentSection);
+    var currentCount = counts.count;
+    var totalImagesInSection = counts.total;
+
+    // Ajoutez le nombre total d'images de la section actuelle au total
+    totalImages += totalImagesInSection;
+
+    // Affichez les résultats dans le HTML
+    var currentResultElement = document.getElementById(currentSectionName + '-result');
+    if (currentResultElement) {
+        currentResultElement.textContent += " " + currentCount + " sur " + totalImagesInSection;
+    } else {
+        console.error("L'élément avec l'ID " + currentSectionName + "-result n'a pas été trouvé.");
     }
+
+    // Affichez les résultats dans la console
+    console.log("Nombre - section " + currentSectionName + " : " + currentCount + " sur " + totalImagesInSection);
+
+}
+
+// Affichez le nombre total d'images dans la console
+console.log("Nombre total d'images : " + totalImages);
+
 });
